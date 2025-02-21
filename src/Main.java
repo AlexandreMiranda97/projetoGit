635a1ffcf1a41288b3428633a3c28963b4f6f86e
Alécio Vieira <alecio.programador@gmail.com>
2025-02-20 22:08:59 -0300

    Nova feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Salve, salve. Alexandre Miranda!");
+        System.out.println("Cê ta doooidooooo!");
     }
 }
